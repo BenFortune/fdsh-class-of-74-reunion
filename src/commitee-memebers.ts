6c73a0da701da5d97da029972abb539a7d11f455
Ben Fortune <ben.fortune@gmail.com>
2023-09-17 18:15:16 -0500

    Initial commit

---
+++ src/commitee-memebers.ts
@@ -0,0 +1,51 @@
+interface MemberInfo {
+    name: string,
+    email: string
+}
+
+export const COMMITTEE_MEMBERS: MemberInfo[] = [
+    {
+        name: 'Bill Schultz',
+        email: 'schultz_bm@bluevalley.net'
+    },
+    {
+        name: 'Charla Green Wilke',
+        email: 'crgreen939@gmail.com'
+    },
+    {
+        name: 'Gwenda Benson Naylor',
+        email: 'gwenda.miata@lcloud.com'
+    },
+    {
+        name: 'Jeanine Fuller Nemitz',
+        email: 'jeanine912@frontiernet.net'
+    },
+    {
+        name: 'Jim Rodenborn',
+        email: 'rody0211@gmail.com'
+    },
+    {
+        name: 'Mick Rathermel',
+        email: 'rathermel@mediacombb.net'
+    },
+    {
+        name: 'Neil VanGundy',
+        email: 'neilvangundy@aol.com'
+    },
+    {
+        name: 'Ruth VanderMaten Babbitt',
+        email: 'ruthalarson@hotmail.com'
+    },
+    {
+        name: 'Sandy Bratland',
+        email: 'msbrat2uc@hotmail.com'
+    },
+    {
+        name: 'Sandy Wilson Potratz',
+        email: 'j.potratz@mchsi.com'
+    },
+    {
+        name: 'Scott Fortune',
+        email: 'skfortune@msn.com'
+    }
+]
