59619f0c916bdfaf74013bb8e8a0ddf2c4eed2ce
Ben Fortune <ben.fortune@gmail.com>
2023-09-22 20:41:51 -0500

    Adds astro's sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/static';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
+      enabled: true
     }
-  })
+  }),
+  site: 'https://www.fdsh1974.com/',
+  integrations: [sitemap()]
 });
